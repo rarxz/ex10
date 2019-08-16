a987c3fcbefae74b207624707d1420d2c84a2e06
ISeulbi <lsb880218@gmail.com>
2019-08-16 11:47:23 +0900

    sum메소드 수정 및 테스트 작성

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,18 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		assertEquals(30, Calc.sum(10, 20));
+		assertEquals(50, Calc.sum(30, 20));
+		assertEquals(10, Calc.sum(2, 8));
+	}
+
+}
